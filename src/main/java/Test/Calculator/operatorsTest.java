231a503ca70aa2a997573d5e2970c66ced08241f
SayaliKadam21 <sayalikadam600@gmail.com>
2026-05-12 14:39:37 +0530

    Arithmetic operators test

---
+++ src/main/java/Test/Calculator/operatorsTest.java
@@ -0,0 +1,93 @@
+package Test.Calculator;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class operatorsTest {
+	ChromeDriver driver;
+	public static void main(String[]args) {
+		operatorsTest test = new operatorsTest();
+		test.launch();
+		test.addition();
+		test.launch();
+		test.sub();
+		test.launch();
+		test.mul();
+		test.launch();
+		test.div();
+		
+	}
+	
+	//Launching ChromeDriver;
+	void launch() {
+		driver = new ChromeDriver();
+		driver.get("https://www.calculator.net/");
+	}
+	
+	
+	//Addition
+	void addition()
+		{
+		
+		driver.findElement(By.xpath("//span[text()='1']")).click();
+		driver.findElement(By.xpath("//span[text()='+']")).click();
+		driver.findElement(By.xpath("//span[text()='2']")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.close();
+	}
+	
+	
+	//Subtraction
+	
+	void sub() {
+		driver.findElement(By.xpath("//span[text()='7']")).click();
+		driver.findElement(By.xpath("//span[text()='8']")).click();
+		driver.findElement(By.xpath("//span[text()='–']")).click();
+		driver.findElement(By.xpath("//span[text()='1']")).click();
+		driver.findElement(By.xpath("//span[text()='0']")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.close();
+	}
+	
+	
+	void mul() {
+		driver.findElement(By.xpath("//span[text()='1']")).click();
+		driver.findElement(By.xpath("//span[text()='0']")).click();
+		driver.findElement(By.xpath("//span[text()='0']")).click();
+		driver.findElement(By.xpath("//span[text()='×']")).click();
+		driver.findElement(By.xpath("//span[text()='5']")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.close();
+	}
+	
+	
+	void div() {
+		driver.findElement(By.xpath("//span[text()='8']")).click();
+		driver.findElement(By.xpath("//span[text()='6']")).click();
+		driver.findElement(By.xpath("//span[text()='4']")).click();
+		driver.findElement(By.xpath("//span[text()='2']")).click();
+		driver.findElement(By.xpath("//span[text()='/']")).click();
+		driver.findElement(By.xpath("//span[text()='1']")).click();
+		driver.findElement(By.xpath("//span[text()='2']")).click();
+	}
+
+}
