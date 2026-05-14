d79ac43394a50773e6b3f5005ed0a5e33e38a64e
SayaliKadam21 <sayalikadam600@gmail.com>
2026-05-14 16:43:18 +0530

    Other functions test

--- src/main/java/Test/Calculator/CalculatorTest.java
+++ src/main/java/Test/Calculator/CalculatorTest.java
@@ -1,10 +1,10 @@
 package Test.Calculator;
 
-public class CalculatorTest implements SinCosTan,PowerFunctions {
+public class CalculatorTest implements SinCosTan,PowerFunctions, otherFunctions1 {
 	public static void main(String[]args) {
 		SinCosTan.sincostan();
 		PowerFunctions.powerfunctions();
-	
+		otherFunctions1.of1();
 		
 	}
 
